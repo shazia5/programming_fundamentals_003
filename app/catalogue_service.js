00407a3df3919096063cb9c3be9ab8a9ebe1573c
Shazia <n_kirmani@yahoo.com>
2019-10-07 23:03:28 +0100

    check book js not completed

--- app/catalogue_service.js
+++ app/catalogue_service.js
@@ -1,50 +1,105 @@
+const Book = require("./Book");
 // eslint-disable-next-line no-unused-vars
 const catalogue = [
-  "The Catcher in the Rye by J.D. Salinger (10)",
-  "Dracula by Bram Stoker (0)",
-  "Between the Assassinations by Aravind Adiga (9)",
-  "Wolf Hall by Hilary Mantel (33)",
-  "Bring Up The Bodies by Hilary Mantel (31)",
-  "A Place of Greater Safety by Hilary Mantel (11)",
-  "Giving Up the Ghost by Hilary Mantel (8)",
-  "The Assassination of Margaret Thatcher by Hilary Mantel (43)",
-  "The Yellow Wallpaper by Charlotte Perkins Gilman (12)",
-  "Conversations With Friends by Sally Rooney (1)",
-  "Normal People by Sally Rooney (2)",
-  "Everything I Never Told You by Celeste Ng (6)",
-  "2666 by Robert Bolaño (17)",
-  "By Night In Chile by Robert Bolaño (8)",
-  "A Tale of Two Cities by Charles Dickens (3)",
-  "Oliver Twist by Charles Dickens (7)",
-  "Great Expectations by Charles Dickens (1)",
-  "The Blind Assassin by Margaret Atwood (8)",
-  "Why Be Happy When You Could Be Normal? by Jeanette Winterson (19)",
-  "The Origin of Species by Charles Darwin (50)",
+    new Book("The Catcher in the Rye", "J.D. Salinger", 10),
+    new Book("Dracula", "Bram Stoker", 0),
+    new Book("Between the Assassinations", "Aravind Adiga", 9),
+    new Book("Wolf Hall", "Hilary Mantel", 33),
+    new Book("Bring Up The Bodies", "Hilary Mantel", 31),
+    new Book("A Place of Greater Safety", "Hilary Mantel", 11),
+    new Book("Giving Up the Ghost", "Hilary Mantel", 8),
+    new Book("The Assassination of Margaret Thatcher", "Hilary Mantel", 43),
+    new Book("The Yellow Wallpaper", "Charlotte Perkins Gilman", 12),
+    new Book("Conversations With Friends", "Sally Rooney", 1),
+    new Book("Normal People", "Sally Rooney", 2),
+    new Book("Everything I Never Told You", "Celeste Ng", 6),
+    new Book("The Origin of Species", "Charles Darwin", 50),
+    new Book("Why Be Happy When You Could Be Normal?", "Jeanette Winterson", 19),
+    new Book("The Blind Assassin", "Margaret Atwood", 8),
+    new Book("Great Expectations" ,"Charles Dickens", 1),
+    new Book("Oliver Twist" ,"Charles Dickens", 7),
+    new Book("A Tale of Two Cities", "Charles Dickens", 3),
+    new Book("By Night In Chile", "Robert Bolaño", 8),
+    new Book("2666", "Robert Bolaño", 17)
 ];
 
 function checkBook(title) {
   if (!title) throw new Error("Please provide a title");
-  // Your code here
+  let result = false;
+  for (let i = 0; i < catalogue.length; i++) {
+   let ct = catalogue[i].title;
+   ct = ct.toLowerCase();
+   const t = title.toLowerCase();
+   if(ct.includes(t)){
+     result = true;
+   }
+  }
+  return result;
 }
 
 function countBooksByKeyword(keyword) {
   if (!keyword) throw new Error("Please provide a keyword");
-  // Your code here
+  let cont = 0;
+  for (let i = 0; i < catalogue.length; i++) {
+    const book = keyword.toLowerCase();
+    let cat = catalogue[i].title;
+    cat = cat.toLowerCase();
+    if (cat.includes(book)) {
+      cont++;
+    }
+  }
+  return cont;
 }
 
 function getBooksByAuthor(author) {
   if (!author) throw new Error("Please provide an author");
-  // Your code here
+  let count = [];
+  for (let i = 0; i < catalogue.length; i++){
+    let at = catalogue[i].author;
+    at = at.toLowerCase();
+    let a = author.toLowerCase();
+    if (at.includes(a)){
+      count.push(catalogue[i].title);
+    }
+  }
+  return count;
 }
 
 function getStockCount(title) {
   if (!title) throw new Error("Please provide a title");
-  // Your code here
+  let result = 0;
+  for (let i = 0; i < catalogue.length; i++) {
+   let ct = catalogue[i].title;
+   ct = ct.toLowerCase();
+   const t = title.toLowerCase();
+   if(ct.includes(t)){
+     result = catalogue[i].quantity;
+   }
+  }
+  return result;
 }
 
 function stockReview(title) {
   if (!title) throw new Error("Please provide a title");
-  // Your code here
+  let result = "Book not found";
+  for (let i = 0; i < catalogue.length; i++) {
+   let ct = catalogue[i].title;
+   ct = ct.toLowerCase();
+   const t = title.toLowerCase();
+   if(ct.includes(t)){
+    let quantity = catalogue[i].quantity;
+    if(quantity > 10){
+      result = "High Stock";
+    } else if (quantity <= 10 && quantity > 5){
+      result =  "Medium Stock";
+    } else if (quantity <= 5 && quantity > 0){
+      result = "Low Stock";
+    } else {
+      result =  "Not in Stock";
+    }
+   }
+  }
+  return result;
 }
 
 module.exports = {
--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -1,5 +1,4 @@
 const catalogueService = require("../app/catalogue_service");
-
 describe("catalogueService", () => {
   describe("catalogueService.checkBook", () => {
     test("returns true when a book is found by title", () => {
@@ -16,6 +15,68 @@ describe("catalogueService", () => {
 
     test("returns false when not even a partial match is found", () => {
       expect(catalogueService.checkBook("The Wind")).toBe(false);
+    });
+  });
+  describe("catalogueService.countBooksByKeyword", () => {
+    test("returns assassin when passed 3", () => {
+      expect(catalogueService.countBooksByKeyword("assassin")).toBe(3);
+    });
+    test("returns normal when passed 2", () => {
+      expect(catalogueService.countBooksByKeyword("normal")).toBe(2);
+    });
+    test("returns pineapple when passed 0", () => {
+      expect(catalogueService.countBooksByKeyword("pineapple")).toBe(0);
+    });
+  });
+  describe("catalogueService.getBooksByAuthor", () => {
+    test("returns A Tale of Two Cities, Oliver Twist, Great Expectations", () => {
+      expect(catalogueService.getBooksByAuthor("Charles Dickens")).toEqual([
+        "Great Expectations",
+      "Oliver Twist",
+      "A Tale of Two Cities"
+      ]);
+    });
+    test("returns an empty Array", () => {
+      expect(catalogueService.getBooksByAuthor("J.R.R. Tokien")).toEqual([]);
+    });
+    test("returns A Tale of Two Cities, Oliver Twist, Great Expectations, The Origin of Species", () => {
+      expect(catalogueService.getBooksByAuthor("Charles")).toEqual([
+        "The Origin of Species",
+        "Great Expectations",
+        "Oliver Twist",
+        "A Tale of Two Cities"
+      ]);
+    });
+  });
+  describe("catalogueService.getStockCount", () => {
+    test("returns Between the Assassinations when passed 9", () => {
+      expect(catalogueService.getStockCount("Between the Assassinations")).toBe(9);
+    });
+    test("returns A Tale of Two Cities when passed 3", () => {
+      expect(catalogueService.getStockCount("A Tale of Two Cities")).toBe(3);
+    });
+    test("returns The Great Gatsbys when passed 0", () => {
+      expect(catalogueService.getStockCount("The Great Gatsby")).toBe(0);
+    });
+    test("returns Dracula when passed 0", () => {
+      expect(catalogueService.getStockCount("Dracula")).toBe(0);
+    });
+  });
+  describe("catalogueService.stockReview", () => {
+    test("Dracula = Not in Stock", () => {
+      expect(catalogueService.stockReview("Dracula")).toBe("Not in Stock");
+    });
+    test("Great Expectations = Low Stock", () => {
+      expect(catalogueService.stockReview("Great Expectations")).toBe("Low Stock");
+    });
+    test("The Blind Assassin = Medium Stock", () => {
+      expect(catalogueService.stockReview("The Blind Assassin")).toBe("Medium Stock");
+    });
+    test("The Yellow Wallpaper = High Stock", () => {
+      expect(catalogueService.stockReview("The Yellow Wallpaper")).toBe("High Stock");
+    });
+    test("The Streets = Book not founded", () => {
+      expect(catalogueService.stockReview("The Streets")).toBe("Book not found");
     });
   });
 });
