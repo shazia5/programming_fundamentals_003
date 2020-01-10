c4463638525cc172d14ee740cf575b62a7b87361
Shazia <n_kirmani@yahoo.com>
2020-01-10 17:02:15 +0000

    F3 all done

--- test/Book.test.js
+++ test/Book.test.js
@@ -1,73 +1,83 @@
-const Book = require("../app/Book");
+const catalogueService = require("../app/catalogue_service");
 
-describe("Instantiating a book", () => {
-  test("it creates a book object", () => {
-    const book = new Book("Oryx and Crake", "Margaret Atwood", 10);
+describe("catalogueService", () => {
+  describe("catalogueService.checkBook", () => {
+    test("returns true when a book is found by title", () => {
+      expect(catalogueService.checkBook("Great Expectations")).toBe(true);
+    });
 
-    expect(book.title).toBe("Oryx and Crake");
-    expect(book.author).toBe("Margaret Atwood");
-    expect(book.quantity).toBe(10);
+    test("returns false when a book cannot be found by title", () => {
+      expect(catalogueService.checkBook("Gone With The Wind")).toBe(false);
+    });
+
+    test("returns true when a partial match is found", () => {
+      expect(catalogueService.checkBook("Expectations")).toBe(true);
+    });
+
+    test("returns false when not even a partial match is found", () => {
+      expect(catalogueService.checkBook("The Wind")).toBe(false);
+    });
   });
-});
-
-xdescribe("addStock()", () => {
-  test("allows adding a given quantity of stock", () => {
-    const book = new Book("Oryx and Crake", "Margaret Atwood", 10);
-
-    expect(book.quantity).toBe(10);
-    
-    // We add 5 more books
-    book.addStock(5);
-    
-    expect(book.quantity).toBe(15);
+  describe("catalogueService.countBooksByKeyword", () => {
+    test("returns assassin when passed 3", () => {
+      expect(catalogueService.countBooksByKeyword("assassin")).toBe(3);
+    });
+    test("returns normal when passed 2", () => {
+      expect(catalogueService.countBooksByKeyword("normal")).toBe(2);
+    });
+    test("returns pineapple when passed 0", () => {
+      expect(catalogueService.countBooksByKeyword("pineapple")).toBe(0);
+    });
   });
-
-  test("does not allow negative numbers", () => {
-    const book = new Book("Oryx and Crake", "Margaret Atwood", 10);
-
-    expect(book.quantity).toBe(10);
-    
-    // Try adding a negative number of stock
-    book.addStock(-5);
-    
-    // Book quantity has not changed
-    expect(book.quantity).toBe(10);
+  describe("catalogueService.getBooksByAuthor", () => {
+    test("returns A Tale of Two Cities, Oliver Twist, Great Expectations", () => {
+      expect(catalogueService.getBooksByAuthor("Charles Dickens")).toEqual([
+        "Great Expectations",
+      "Oliver Twist",
+      "A Tale of Two Cities"
+      ]);
+    });
+    test("returns an empty Array", () => {
+      expect(catalogueService.getBooksByAuthor("J.R.R. Tokien")).toEqual([]);
+    });
+    test("returns A Tale of Two Cities, Oliver Twist, Great Expectations, The Origin of Species", () => {
+      expect(catalogueService.getBooksByAuthor("Charles")).toEqual([
+        "The Origin of Species",
+        "Great Expectations",
+        "Oliver Twist",
+        "A Tale of Two Cities"
+      ]);
+    });
   });
-});
-
-xdescribe("removeStock()", () => {
-  test("allows removing a given quantity of stock", () => {
-    const book = new Book("Oryx and Crake", "Margaret Atwood", 10);
-
-    expect(book.quantity).toBe(10);
-    
-    // We remove 5 books
-    book.removeStock(3);
-    
-    expect(book.quantity).toBe(7);
+  describe("catalogueService.getStockCount", () => {
+    test("returns Between the Assassinations when passed 9", () => {
+      expect(catalogueService.getStockCount("Between the Assassinations")).toBe(9);
+    });
+    test("returns A Tale of Two Cities when passed 3", () => {
+      expect(catalogueService.getStockCount("A Tale of Two Cities")).toBe(3);
+    });
+    test("returns The Great Gatsbys when passed 0", () => {
+      expect(catalogueService.getStockCount("The Great Gatsby")).toBe(0);
+    });
+    test("returns Dracula when passed 0", () => {
+      expect(catalogueService.getStockCount("Dracula")).toBe(0);
+    });
   });
-
-  test("does not allow negative numbers", () => {
-    const book = new Book("Oryx and Crake", "Margaret Atwood", 10);
-
-    expect(book.quantity).toBe(10);
-    
-    // Try removing a negative number of stock
-    book.removeStock(-5);
-    
-    // Book quantity has not changed
-    expect(book.quantity).toBe(10);
-  });
-
-  test("cannot reduce a book to below 0 quantity", () => {
-    const book = new Book("Oryx and Crake", "Margaret Atwood", 10);
-
-    expect(book.quantity).toBe(10);
-    
-    // Try removing 15 books
-    book.removeStock(15);
-    
-    // Book quantity is 0
-    expect(book.quantity).toBe(0);
+  describe("catalogueService.stockReview", () => {
+    test("Dracula = Not in Stock", () => {
+      expect(catalogueService.stockReview("Dracula")).toBe("Not in Stock");
+    });
+    test("Great Expectations = Low Stock", () => {
+      expect(catalogueService.stockReview("Great Expectations")).toBe("Low Stock");
+    });
+    test("The Blind Assassin = Medium Stock", () => {
+      expect(catalogueService.stockReview("The Blind Assassin")).toBe("Medium Stock");
+    });
+    test("The Yellow Wallpaper = High Stock", () => {
+      expect(catalogueService.stockReview("The Yellow Wallpaper")).toBe("High Stock");
+    });
+    test("The Streets = Book not founded", () => {
+      expect(catalogueService.stockReview("The Streets")).toBe("Book not found");
+    });
   });
 });
